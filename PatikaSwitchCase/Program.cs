117d89d2a4a37cc4b1b7ca39ec9f9766489a387b
Emre Pirvan <emreprvn@gmail.com>
2022-04-02 05:30:49 +0300

    Add project files.

---
+++ PatikaSwitchCase/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PatikaSwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("ocak ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("subat ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("mart ayındasınız");
+                    break;
+                case 4:
+                    Console.WriteLine("nisan ayındasınız");
+                    break;
+                case 5:
+                    Console.WriteLine("mayıs ayındasınız");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                    break;
+            }
+            switch (month)
+            {
+                case 12:
+                case 2:
+                case 1:
+                    Console.WriteLine("kış ayındasınız");
+                    break;
+                case 3:
+                    case 4:
+                        case 5:
+                    Console.WriteLine("İlk bahar ayındasınız");
+                    break;
+                case 6:
+                    case 7:
+                        case 8:
+                    Console.WriteLine("Yaz ayındasınız");
+                    break;
+                case 9:
+                    case 10:
+                        case 11:
+                    Console.WriteLine("Son bahar ayındasınız");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
